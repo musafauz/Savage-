cce4b1195230d06825bc9cb7a44b80b1478e71ec
musafauz <musafauz761@gmail.com>
2025-07-05 23:29:14 +0300

    Update set.js

--- set.js
+++ set.js
@@ -9,8 +9,8 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || '',
     PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "xh_clinton",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || "254735342808",              
+    OWNER_NAME: process.env.OWNER_NAME || "Musa ",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "256784079946",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_READ_MESSAGES: process.env.AUTO_READ_MESSAGES || "yes",       
     AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS || "yes",                     
